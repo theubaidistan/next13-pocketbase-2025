0781caba1ae7494d11262a32843201ef052f557e
theubaidistan <ubaidistan395@gmail.com>
2025-12-30 08:08:55 +0500

    2

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,15 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning>
+      <head>
+        <title>My Notes App</title>
+        <meta
+          name="description"
+          content="This is a detailed description of my Full Stack Application."
+        />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body suppressHydrationWarning>
         <main>
           <nav>
--- lib/pocketbase.ts
+++ lib/pocketbase.ts
@@ -1,6 +1,6 @@
 // lib/pocketbase.js
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("https://your-pocketbase.onrender.com");
+const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
 
 export default pb;
